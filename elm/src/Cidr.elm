088bc671aff88d9bf2d86caae6e1bfcae26d6524
Erik Schmidt <3rik.schmidt@gmail.com>
2017-03-05 18:05:56 -0500

    reformatting

--- elm/src/Cidr.elm
+++ elm/src/Cidr.elm
@@ -1,31 +1,44 @@
-module Cidr exposing ( .. )
+module Cidr exposing (..)
+
 import Bitwise exposing (..)
 
+
 ip42int : String -> Int
-ip42int = String.split "."
-  >> List.map (String.toInt >> Result.withDefault 0)
-  >> List.foldl (\a b -> or (and a 0xff) (shiftLeftBy 8 b)) 0
+ip42int =
+    String.split "."
+        >> List.map (String.toInt >> Result.withDefault 0)
+        >> List.foldl (\a b -> or (and a 0xFF) (shiftLeftBy 8 b)) 0
+
 
 bits2mask maskBits =
-    shiftLeftBy (32 - maskBits) 0xffffffff
+    shiftLeftBy (32 - maskBits) 0xFFFFFFFF
+
 
 int2ip4 ip =
-  String.join "." (List.map toString [
-    shiftRightZfBy 24 ip |> and 0xff,
-    shiftRightZfBy 16 ip |> and 0xff,
-    shiftRightZfBy  8 ip |> and 0xff,
-    and 0xff ip
-  ])
+    String.join "."
+        (List.map toString
+            [ shiftRightZfBy 24 ip |> and 0xFF
+            , shiftRightZfBy 16 ip |> and 0xFF
+            , shiftRightZfBy 8 ip |> and 0xFF
+            , and 0xFF ip
+            ]
+        )
 
-cidr : String -> Int -> (String, String)
-cidr ip bits = cidrCalc (ip42int ip) (bits2mask bits)
 
-{-}    /**
-     * @param {number} ip
-     * @param {number}  mask
-     * @returns {[string,string]}
-     */
+cidr : String -> Int -> ( String, String )
+cidr ip bits =
+    cidrCalc (ip42int ip) (bits2mask bits)
+
+
+
+{- }    /**
+   * @param {number} ip
+   * @param {number}  mask
+   * @returns {[string,string]}
+   */
 -}
-cidrCalc : Int -> Int -> (String, String)
+
+
+cidrCalc : Int -> Int -> ( String, String )
 cidrCalc ip mask =
- (int2ip4 (and ip mask), int2ip4 (or ip (complement mask)))
+    ( int2ip4 (and ip mask), int2ip4 (or ip (complement mask)) )
--- elm/src/main.elm
+++ elm/src/main.elm
@@ -8,83 +8,113 @@ import Json.Decode
 import Regex exposing (regex, contains)
 import Cidr exposing (..)
 
+
 main =
-  Html.beginnerProgram {
-    model = model,
-    view = view,
-    update = update
-  }
+    Html.beginnerProgram
+        { model = model
+        , view = view
+        , update = update
+        }
+
 
 
 -- MODEL
-type alias Bitmask = {mask: String, bits: Int}
 
--- bits2mask : Bits -> Bitmask
+
+type alias Bitmask =
+    { mask : String, bits : Int }
+
+
 bitMask : Int -> Bitmask
-bitMask x = Bitmask (Cidr.bits2mask x |> Cidr.int2ip4) x
+bitMask x =
+    Bitmask (Cidr.bits2mask x |> Cidr.int2ip4) x
+
 
 bitMasks : List Bitmask
-bitMasks = List.map bitMask <| List.reverse <| List.range 1 32
+bitMasks =
+    List.map bitMask <| List.reverse <| List.range 1 32
+
 
 type alias Model =
-  {
-    ip : String,
-    bits : Int
-  }
+    { ip : String
+    , bits : Int
+    }
+
 
 model : Model
 model =
-  Model "" 0
+    Model "" 0
+
+
 
 -- UPDATE
 
 
 type Msg
-  = IpUpdated String
-  | BitsUpdated String
+    = IpUpdated String
+    | BitsUpdated String
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    IpUpdated newIp ->
-      { model | ip = newIp }
-    BitsUpdated newBits ->
-      { model | bits = String.toInt newBits |> Result.withDefault -1 }
+    case msg of
+        IpUpdated newIp ->
+            { model | ip = newIp }
+
+        BitsUpdated newBits ->
+            { model | bits = String.toInt newBits |> Result.withDefault -1 }
+
+
 
 -- VIEW
-br = Html.br [] []
+
+
+br =
+    Html.br [] []
+
 
 bitmask2option : Bitmask -> Html msg
 bitmask2option bm =
-  let
-    stringBits = toString bm.bits
-  in
-    option [title stringBits, value stringBits] [text <| stringBits ++ "   /    " ++ bm.mask]
+    let
+        stringBits =
+            toString bm.bits
+    in
+        option [ title stringBits, value stringBits ] [ text <| stringBits ++ "   /    " ++ bm.mask ]
+
 
 onChange tagger =
-  Html.Events.on "change" (Json.Decode.map tagger Html.Events.targetValue)
+    Html.Events.on "change" (Json.Decode.map tagger Html.Events.targetValue)
+
 
 view : Model -> Html Msg
 view model =
-  let
-    bitMaskOptions = List.map bitmask2option bitMasks
-  in
-    div []
-      [ text "CIDR Calculator", br,
-        input [ placeholder "1.2.3.4", onInput IpUpdated ] [],
-        text " with ",
-        select [ name "bits", title "bits", onChange BitsUpdated ] ((option [ value "" ] [ text "bits | mask" ]) :: bitMaskOptions),
-        br, br,
-        div [ id "output", title "output" ] [ text (doCidr model) ]
-      ]
+    let
+        bitMaskOptions =
+            option [ value "" ] [ text "bits | mask" ]
+                :: List.map bitmask2option bitMasks
+    in
+        div []
+            [ text "CIDR Calculator"
+            , br
+            , input [ placeholder "1.2.3.4", onInput IpUpdated ] []
+            , text " with "
+            , select [ name "bits", title "bits", onChange BitsUpdated ] bitMaskOptions
+            , br
+            , br
+            , div [ id "output", title "output" ] [ text (doCidr model) ]
+            ]
+
 
 doCidr model =
-  if (isValidIp model.ip) && (model.bits >= 0) then
-    let
-      (low, high) = (cidr model.ip model.bits)
-    in
-      low ++ " - " ++ high
-  else
-    "invalid input"
+    if (isValidIp model.ip) && (model.bits >= 0) then
+        let
+            ( low, high ) =
+                (cidr model.ip model.bits)
+        in
+            low ++ " - " ++ high
+    else
+        "invalid input"
 
-isValidIp str = contains (regex "^(?!\\.)(\\.?[0-9]{1,3}){4}$") str
+
+isValidIp str =
+    contains (regex "^(?!\\.)(\\.?[0-9]{1,3}){4}$") str
